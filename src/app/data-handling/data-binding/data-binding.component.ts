4bc6d5942c709e5421d388f325d96f7aab65496c
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2021-11-24 14:16:40 +0530

    Attribute binding completed

--- src/app/data-handling/data-binding/data-binding.component.ts
+++ src/app/data-handling/data-binding/data-binding.component.ts
@@ -9,6 +9,11 @@ export class DataBindingComponent implements OnInit {
   title='Data Binding Examples';
   description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, natus?';
   descriptionBold='<b>Lorem ipsum</b> dolor sit amet consectetur adipisicing elit. Beatae, natus?';
+  imgSrc="https://dummyimage.com/300x200/3030c7/fff.png";
+  rowSpan=3;
+  colSpan=2;
+
+  imgAlt="Dummy Image";
   constructor() { }
 
   ngOnInit(): void {
